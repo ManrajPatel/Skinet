ffa0b22b2b901a818609b6e52113f868a15330ea
ManrajPatel <mnrj.patel@gmail.com>
2020-05-11 21:14:48 +0530

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            // This code will generate a SQL SELECT statement in background to get data from database
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("GetP")] // User cannot make call with method name. Call has to be made through route name
+        /*
+         * if no route is mentioned and there are 2 Get methods then in that case calling the api with only controller route will 
+         * generate an exception
+         */
+        public async Task<ActionResult<List<Product>>> GetProducts1()
+        {
+            // This code will generate a SQL SELECT statement in background to get data from database
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
